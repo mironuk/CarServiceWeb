75c27bb31854f214ca8e1a492b7d4d8cd1bf7a99
Sergei Batiuk <sergei.batiuk@hitech.com.ua>
2018-11-23 00:30:34 +0200

    CAR-1 Add Spring Boot Infrastructure
    - Added the first service test
    - Added the @Repository annotation to the JPA repositories
    - Added the CarServiceTx transactional class.

--- src/main/java/carservice/web/service/CarService.java
+++ src/main/java/carservice/web/service/CarService.java
@@ -1,21 +1,35 @@
 package carservice.web.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import carservice.web.data.model.Car;
 import carservice.web.data.model.ServiceItem;
+import carservice.web.data.repository.CarRepository;
+import carservice.web.service.tx.CarServiceTx;
 
 @Service
 public class CarService {
 
+	@Autowired
+	private CarServiceTx carServiceTx;
+
+	@Autowired
+	private CarRepository carRepository;
+
 	public void createCar(Car car) {
-		
+		carServiceTx.createCar(car);
 	}
 
 	public List<Car> getCars() {
-		return null;
+		return carRepository.findAll();
+	}
+
+	public Optional<Car> getCar(long carId) {
+		return carRepository.findById(carId);
 	}
 
 	public void createServiceItem(ServiceItem serviceItem) {
---
+++ src/test/java/carservice/web/service/CarServiceTest.java
@@ -0,0 +1,52 @@
+package carservice.web.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import carservice.web.data.model.Car;
+import carservice.web.data.repository.CarRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarServiceTest {
+
+	@Autowired
+	private CarRepository carRepository;
+
+	@Autowired
+	private CarService carService;
+
+	@Before
+	public void init() {
+		carRepository.deleteAll();
+	}
+
+	@Test
+	public void testCreateCar() {
+		// Arrange
+		Car car = new Car();
+		car.setMake("Mazda");
+		car.setModel("3");
+		car.setYear(2004);
+
+		// Act
+		carService.createCar(car);
+
+		// Assert
+		long carId = car.getCarId();
+		Car testCar = carService.getCar(carId).get();
+		assertNotNull(testCar);
+
+		assertEquals("Mazda", testCar.getMake());
+		assertEquals("3", testCar.getModel());
+		assertEquals(2004, testCar.getYear());
+	}
+
+}
