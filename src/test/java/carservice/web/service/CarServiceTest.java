61b36a5eac0832075e87ebd6d36686b3bc28b961
Roman Mironuk <mironuk@gmail.com>
2018-11-27 03:39:21 +0200

    CAR-8 : CarServiceTest affects dev_carservice database
    - Possible fix.

--- src/test/java/carservice/web/service/CarServiceTest.java
+++ src/test/java/carservice/web/service/CarServiceTest.java
@@ -8,12 +8,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import carservice.web.data.model.Car;
 import carservice.web.data.model.User;
 import carservice.web.data.repository.CarRepository;
 
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CarServiceTest {
